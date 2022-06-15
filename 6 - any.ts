e40f17ac2dd67a001f260ad51a16d2bd3b4bc889
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-06-15 15:52:33 +0200

    reorganise and better TS project setup

--- 6 - any.ts
+++
@@ -1,4 +0,0 @@
-// TODO: 21 - Working with any type
-/**
- * simply do not use any TS Type if you do not need to,  coz it will take you back to Vanilla JS World === you lose all the TS benefits
- */
---
+++ src/app.ts
@@ -0,0 +1,3 @@
+let user: string = 'zack';
+
+user = 'kay';
