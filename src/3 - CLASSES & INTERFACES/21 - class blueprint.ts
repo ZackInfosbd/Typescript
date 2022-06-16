f47dbbed5667390954fe638be3e36a81a9847ea9
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-06-16 07:20:09 +0200

    TS Class blueprint + contsructor function + this keyword

---
+++ src/3 - CLASSES & INTERFACES/21 - class blueprint.ts
@@ -0,0 +1,21 @@
+// class Department {
+//   id: number;
+//   name: string;
+
+//   constructor(n: string, num: number) {
+//     this.id = num;
+//     this.name = n;
+//   }
+// }
+
+// const departmentObj = new Department('IT', 1);
+
+// console.log(departmentObj);
+// console.log(departmentObj.name);
+// console.log(departmentObj.id);
+
+/**
+ * the classes are the blueprint of the objects used programatically, we use classes to make coppies or instances of those objects wherever nedded.
+ * the class syntax is the syntax sugar for constructor functions
+ * the class has fields and methods which will be constructed to object properties and methods
+ */
---
+++ src/3 - CLASSES & INTERFACES/22 - constructor function & this keyword.ts
@@ -0,0 +1,35 @@
+class Department {
+  id: number;
+  departmentName: string;
+  constructor(num: number, name: string) {
+    this.id = num;
+    this.departmentName = name;
+  }
+
+  describe(this: Department) {
+    console.log(
+      `Department number is: ${this.id}, which is under the name of ${this.departmentName}`
+    );
+  }
+}
+
+const department = new Department(1, 'IT');
+
+console.log(department);
+console.log(department.id);
+console.log(department.departmentName);
+
+department.describe();
+
+const AccountingDep = {
+  id: 2,
+  departmentName: 'Accounting',
+  describe: department.describe,
+};
+
+AccountingDep.describe();
+
+/**
+ * do not forget to use the keyword this while building your objects even with classes,otherwise the field:(property-later) will point to any global variable with that name
+ * you can pass the kewyword this to a method as a param with type of the class name,that will help if you will assign that method later on to an other object, because you know the keyword this is so tricky.
+ */
