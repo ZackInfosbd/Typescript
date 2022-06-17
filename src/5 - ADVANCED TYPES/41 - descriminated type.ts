a2c5a77e6608413066aa54890e8553c0552ac514
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-06-17 05:08:41 +0200

    TS Descriminated type for objects and union types

---
+++ src/5 - ADVANCED TYPES/41 - descriminated type.ts
@@ -0,0 +1,44 @@
+// interface Bird {
+//   gender: 'bird';
+//   flyingSpeed: number;
+// }
+// interface Horse {
+//   gender: 'horse';
+//   runningSpeed: number;
+// }
+
+// type Animal = Bird | Horse;
+
+// function moveAnimal1(animal: Animal) {
+//   let speed;
+//   if (animal.gender === 'bird') {
+//     speed = animal.flyingSpeed;
+//   }
+//   if (animal.gender === 'horse') {
+//     speed = animal.runningSpeed;
+//   }
+//   console.log('From IF-statement:Moving with speed: ' + speed);
+// }
+
+// function moveAnimal2(animal: Animal) {
+//   let speed;
+//   switch (animal.gender) {
+//     case 'bird':
+//       speed = animal.flyingSpeed;
+//       break;
+//     case 'horse':
+//       speed = animal.runningSpeed;
+//       break;
+//   }
+//   console.log('From SWITCH-statement:Moving with speed: ' + speed);
+// }
+
+// moveAnimal1({ gender: 'bird', flyingSpeed: 120 });
+// moveAnimal1({ gender: 'horse', runningSpeed: 80 });
+
+// moveAnimal2({ gender: 'bird', flyingSpeed: 120 });
+// moveAnimal2({ gender: 'horse', runningSpeed: 80 });
+
+/**
+ * Descriminated union: is a pattern which we can use when we work with union types and all is about checking the property which is in comon for both interfaces/object to diffrenciate them and in place of give type string for axample we can narrow it to more specified value like we did in our example, that makes implementing type guards easier!!! and also eliminate the danger of mistyping , it is available when you working with objects and union types;
+ */
