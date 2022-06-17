975858b9ad8ec930be211b9eb09f6497867904df
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-06-17 07:06:00 +0200

    TS optional chaining

---
+++ src/5 - ADVANCED TYPES/45 - optional chaining.ts
@@ -0,0 +1,17 @@
+// const fetchedUserData = {
+//   id: 'u1',
+//   name: 'zack',
+//   job: { title: 'CEO', description: 'My own company' },
+// };
+
+// // .1
+// // console.log(fetchedUserData.job && fetchedUserData.job.title);
+
+// // .2
+// console.log(fetchedUserData?.job?.title);
+
+/**
+ * Optional chaining:
+ * scenario: lets say that we re getting some fetched data from a resourse, but we dont know if in a object a certain property is defined!!!; maybe we dont fetch all the data
+ * 1. optional chaining in JS
+ */
