e676d15cc505d998cdb1ec7a7ad2d29581d49751
ZackInfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2022-06-17 13:04:14 +0200

    TS Generic Types vs Union Types

---
+++ src/6 - Generics/54 - genric vs union types.ts
@@ -0,0 +1,4 @@
+/**
+ * Union types can be great if you want to have a function, which you can call with one of these types every time you call it.
+ * generic types are great if you want to lock in a certain type, used the same type throughout the entire class instance,
+ */
