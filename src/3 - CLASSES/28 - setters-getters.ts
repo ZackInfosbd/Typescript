482726d4c13e8adc8cb2d788d224f0f8ec4cf136
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-06-22 20:00:53 +0200

    refactor: setters and getters

--- src/3 - CLASSES/28 - setters-getters.ts
+++ src/3 - CLASSES/28 - setters-getters.ts
@@ -1,75 +1,110 @@
-// class Department7 {
-//   private readonly id: number;
-//   private name: string;
-//   protected employees: string[] = [];
-//   constructor(id: number, name: string) {
-//     this.id = id;
-//     this.name = name;
+// class Department {
+//   constructor(
+//     private readonly id: number,
+//     protected name: string,
+//     private employees: string[]
+//   ) {}
+
+//   describe(this: Department) {
+//     console.log(`Department ${this.id} ${this.name}`);
 //   }
 
-//   describe(this: Department7) {
-//     console.log(
-//       `the Department ${this.name}, which is under the number ${this.id}`
-//     );
-//   }
-
-//   printInfosEmployee(employee: string) {
+//   addEmployee(employee: string) {
 //     this.employees.push(employee);
-//     console.log(this.employees);
+//     console.log(`the new added employee is: ${employee} `);
+//   }
+
+//   printEmployeeInformations(this: Department) {
+//     console.log(
+//       `The number of the employees of ${this.name} is: ${this.employees.length}`
+//     );
 //   }
 // }
 
-// class AccountingDepartment2 extends Department7 {
-//   private reports: string[];
-//   private lastReport: string;
+// const accountinfDepartment = new Department(1, 'Accounting', []);
 
-//   constructor(id: number, reports: string[]) {
-//     super(id, 'Accounting');
-//     this.reports = reports;
-//     this.lastReport = reports[0];
+// accountinfDepartment.addEmployee('Zack');
+
+// accountinfDepartment.describe();
+
+// accountinfDepartment.printEmployeeInformations();
+
+// class ITDepartment extends Department {
+//   constructor(
+//     id: number,
+//     private admins: string[],
+//     private reports: string[],
+//     private lastReport: string = reports[0]
+//   ) {
+//     super(id, 'IT', []);
 //   }
 
 //   get mostRecentReport() {
 //     if (this.lastReport) {
 //       return this.lastReport;
 //     }
-//     throw new Error('No reports found');
+//     throw new Error('NO Report found!');
 //   }
 
 //   set mostRecentReport(value: string) {
-//     if (!value) {
-//       throw new Error('Please pass in a valid value');
-//     }
+//     if (!value) throw new Error('please pass in a valid value');
 //     this.addReport(value);
 //   }
 
-//   addEmployee(name: string) {
-//     if (name === 'Zack') {
-//       return;
-//     }
-//     this.employees.push(name);
+//   addAdmins(admin: string) {
+//     this.admins.push(admin);
+//     console.log(`the new added admin is: ${admin}`);
+//   }
+
+//   getAdmins() {
+//     console.log(
+//       `the number of admins in this department is: ${this.admins.length}`
+//     );
+//   }
+
+//   getDepartmentName() {
+//     console.log(`this is the name : ${this.name}`);
 //   }
 
 //   addReport(text: string) {
 //     this.reports.push(text);
-//     this.lastReport = text;
+//   }
+
+//   getReports() {
 //     console.log(this.reports);
-//     console.log(this.lastReport);
 //   }
 // }
 
-// const test = new AccountingDepartment2(2, []);
-// test.mostRecentReport = 'i am the setter report!!!';
-// console.log(test.mostRecentReport);
+// const ITDep = new ITDepartment(2, [], ['first report ever']);
+// // ITDep.mostRecentReport = '';
+// ITDep.mostRecentReport = 'this is just an other boring reportS';
 
-// test.describe();
-// test.printInfosEmployee('Ayoub');
-// test.printInfosEmployee('Mansour');
-// test.addReport('i am the last report');
-// console.log(test.mostRecentReport);
+// ITDep.addAdmins('Baba');
+
+// ITDep.getAdmins();
+
+// ITDep.addEmployee('Zack');
+
+// ITDep.printEmployeeInformations();
+
+// ITDep.addEmployee('mansour');
+
+// ITDep.printEmployeeInformations();
+
+// console.log(ITDep.getDepartmentName());
+
+// ITDep.addReport('this is a report');
+
+// ITDep.getReports();
+
+// const theMostRecentReport = ITDep.mostRecentReport;
+// console.log(theMostRecentReport);
 
 /**
+ *  getters and setters,w hich can be great for encapsulating logic,and for adding extra logic, that should run when you try to read a property, or when you try to set a property.
+ 
  * getters - get keyword: is a feature that allow read property and operate on it within more complex logic.
+
  * setters - set keyword:is a feature that allow to set a value
  * getter and setters which can be great for encapsulating logic and for adding extra logic when you try to read a property or to set a property.
  */
--- src/app.ts
+++ src/app.ts
@@ -30,8 +30,25 @@ accountinfDepartment.describe();
 accountinfDepartment.printEmployeeInformations();
 
 class ITDepartment extends Department {
-  constructor(id: number, private admins: string[]) {
+  constructor(
+    id: number,
+    private admins: string[],
+    private reports: string[],
+    private lastReport: string = reports[0]
+  ) {
     super(id, 'IT', []);
+  }
+
+  get mostRecentReport() {
+    if (this.lastReport) {
+      return this.lastReport;
+    }
+    throw new Error('NO Report found!');
+  }
+
+  set mostRecentReport(value: string) {
+    if (!value) throw new Error('please pass in a valid value');
+    this.addReport(value);
   }
 
   addAdmins(admin: string) {
@@ -48,9 +65,19 @@ class ITDepartment extends Department {
   getDepartmentName() {
     console.log(`this is the name : ${this.name}`);
   }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  getReports() {
+    console.log(this.reports);
+  }
 }
 
-const ITDep = new ITDepartment(2, []);
+const ITDep = new ITDepartment(2, [], ['first report ever']);
+// ITDep.mostRecentReport = '';
+ITDep.mostRecentReport = 'this is just an other boring reportS';
 
 ITDep.addAdmins('Baba');
 
@@ -65,3 +92,10 @@ ITDep.addEmployee('mansour');
 ITDep.printEmployeeInformations();
 
 console.log(ITDep.getDepartmentName());
+
+ITDep.addReport('this is a report');
+
+ITDep.getReports();
+
+const theMostRecentReport = ITDep.mostRecentReport;
+console.log(theMostRecentReport);
