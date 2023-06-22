16e2959b1901979ade7403473f25766270615cc2
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-06-22 16:16:10 +0200

    refactor: classes introduction

--- src/3 - CLASSES/21 - class blueprint.ts
+++ src/3 - CLASSES/21 - class blueprint.ts
@@ -15,7 +15,19 @@
 // console.log(departmentObj.id);
 
 /**
+ * Objects are the concrete things we work with in our code, the data structures we use to store data store mothods and execute them.
+ 
  * the classes are the blueprint of the objects used programatically, we use classes to make coppies or instances of those objects wherever nedded.
- * the class syntax is the syntax sugar for constructor functions
+
+ * the class syntax is the syntax sugar for constructor functions.
+
  * the class has fields and methods which will be constructed to object properties and methods
+
+
+  Objects <============================================================================== Classes
+  "The things you work wiith in code"                                  || "Blueprints for objects" (theoritical definition)
+  "Instances of classes (= based on classes)"                          || "Define how objects look like, which properties and methods they have"
+  "class-bbased creation is an alternative to using object literals "  || "Classes make creation of multiple, similar objects much easier"
+
+
  */
