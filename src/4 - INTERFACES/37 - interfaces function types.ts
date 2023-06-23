73d87409572788dc156b6d8b74b1722d0eaccb4a
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2023-06-23 15:38:48 +0200

    refactor: interfaces function types

--- src/4 - INTERFACES/37 - interfaces function types.ts
+++ src/4 - INTERFACES/37 - interfaces function types.ts
@@ -19,6 +19,10 @@
 
 /**
  * we have said that interfaces are used to define the structure of the object
- * interfaces can be used to define the structure of a function, because functions at the end are objects too
- * it s abit different compared to defining methods in objects in interface where we put the name of the method greet(phrase:string):void
+ 
+* interfaces can be used to define the structure of a function, because functions at the end are objects too
+ 
+* it s abit different compared to defining methods in objects in interface where we put the name of the method greet(phrase:string):void.
+ 
+ * using custom types with the functions is a bit common and way shorter.
  */
