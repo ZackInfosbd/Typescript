3d75066c5778bcee96e7f54036c7cb17402f5fbe
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-02-26 12:18:01 +0100

    typescript: tuple and set

---
+++ src/2024/design patterns/TS/set.ts
@@ -0,0 +1,36 @@
+// let a: Set<number>
+// a = new Set([1, 2, 3, 4])
+// let b: Set<string>
+// b = new Set(['a', 'b', 'c', 'd', 'a']) // the second `a` is not added
+// let c: Set<unknown>
+// c = new Set([1, 'b', true])
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+// const a: Set<string> = new Set()
+// // adding items
+// a.add('cat')
+// a.add('dog')
+// a.add('bird')
+// console.log(a)
+
+// // remove an item
+// a.delete('dog')
+// console.log(a)
+
+// // Retrieve an individual item.
+// console.log(Array.from(a)[1])
+
+// // The great thin about a set compared to an array, is that all
+// // items are guaranteed to be unique. No duplicates allowed.
+// a.add('bird')
+// a.add('bird')
+// console.log(a)
+
+// // Get the length of the Set
+// console.log(a.size)
+
+// // Check if a Set has a value
+// console.log(a.has('cat'))
---
+++ src/2024/design patterns/TS/tuple.ts
@@ -0,0 +1,7 @@
+// let a: [number, string]
+// a = [1, 'abc']
+// let b: [string, boolean, number]
+// b = ['abc', false, 123]
+
+// console.log(a[1])
+// console.log(b[2])
