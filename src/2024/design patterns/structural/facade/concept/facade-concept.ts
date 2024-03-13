77039dab9081cd3ae98873380bcbe680eb1959cf
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-03-13 18:58:57 +0100

    facade patterns: client

--- src/2024/design patterns/structural/facade/concept/facade-concept.ts
+++ src/2024/design patterns/structural/facade/concept/facade-concept.ts
@@ -38,3 +38,15 @@ class Facade {
     return new SubSystemClassC().method(value);
   }
 }
+
+// The Client
+// Calling potentially complicated subsystems directly
+console.log(new SubSystemClassA().method());
+console.log(new SubSystemClassB().method('B'));
+console.log(new SubSystemClassC().method({ C: [1, 2, 3] }));
+
+// or using the simplified facade instead
+const FACADE = new Facade();
+console.log(FACADE.subSystemClassA());
+console.log(FACADE.subSystemClassB('B'));
+console.log(FACADE.subSystemClassC({ C: [1, 2, 3] }));
