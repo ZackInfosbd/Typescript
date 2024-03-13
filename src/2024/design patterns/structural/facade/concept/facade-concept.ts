7181825e394da94eb8f84776658a41c034173c9f
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-03-13 18:57:45 +0100

    facade patterns: sub system classe c and facade

--- src/2024/design patterns/structural/facade/concept/facade-concept.ts
+++ src/2024/design patterns/structural/facade/concept/facade-concept.ts
@@ -13,3 +13,28 @@ class SubSystemClassB {
     return value;
   }
 }
+
+class SubSystemClassC {
+  // A hypothetically complicated class
+  method(value: { C: number[] }): { C: number[] } {
+    return value;
+  }
+}
+
+class Facade {
+  // A simplified facade offering the services of subsystems
+  subSystemClassA(): string {
+    // Uses the subsystems method
+    return new SubSystemClassA().method();
+  }
+
+  subSystemClassB(value: string): string {
+    // Uses the subsystems method
+    return new SubSystemClassB().method(value);
+  }
+
+  subSystemClassC(value: { C: number[] }): { C: number[] } {
+    // Uses the subsystems method
+    return new SubSystemClassC().method(value);
+  }
+}
