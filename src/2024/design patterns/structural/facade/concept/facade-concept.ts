74b2893cdd61701b37e0ab525ac5d97123b89cc5
Zakaria <68170594+ZackInfosbd@users.noreply.github.com>
2024-03-13 18:56:36 +0100

    facade patterns: sub system classes

---
+++ src/2024/design patterns/structural/facade/concept/facade-concept.ts
@@ -0,0 +1,15 @@
+// The Facade pattern concept
+
+class SubSystemClassA {
+  // A hypothetically complicated class
+  method(): string {
+    return 'A';
+  }
+}
+
+class SubSystemClassB {
+  // A hypothetically complicated class
+  method(value: string): string {
+    return value;
+  }
+}
