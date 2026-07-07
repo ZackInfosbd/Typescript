58a92baf5972c1d7aa66c4d07e122d7fdf69970a
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-07 13:45:29 +0200

    review(2026): generics - function generics & inference

---
+++ src/2026/6 - GENERICS/50 - Generic funtions & Inference.ts
@@ -0,0 +1,13 @@
+// function merge(a: any, b:any) {
+//     return [a,b]
+// }
+
+// const ids = merge(1, 2)
+// // ids[0].  // // We get no autocompletion
+
+function merge<T>(a: T, b: T) {
+  return [a, b];
+}
+
+// const ids = merge<number>(1, 2) // even better look down next-line
+const ids = merge(1, 2);
