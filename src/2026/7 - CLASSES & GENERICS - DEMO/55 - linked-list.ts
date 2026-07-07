9e68e1eeddd8a856aa83659863f73e95638a378b
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-07 14:33:01 +0200

    review(2026): generics - generic class - DSA - linked list

---
+++ src/2026/7 - CLASSES & GENERICS - DEMO/55 - linked-list.ts
@@ -0,0 +1,52 @@
+// // Building a Linked List Data Structure
+// // Work with classes & generics
+
+// // A data structure that stores connected values or connected values container
+// // Every Node links to the node next to it (but to no other node in the overall list)
+
+// class ListNode<T> {
+//   next?: ListNode<T>;
+
+//   constructor(public value: T) {}
+// }
+
+// class LinkedList<T> {
+//   private root?: ListNode<T>;
+//   private tail?: ListNode<T>;
+//   private length = 0;
+
+//   add(value: T) {
+//     const node = new ListNode(value);
+//     if (!this.root || !this.tail) {
+//       this.root = node;
+//       this.tail = node;
+//     } else {
+//       this.tail.next = node;
+//       this.tail = node;
+//     }
+//     this.length++;
+//   }
+
+//   getNumberOfElements() {
+//     return this.length;
+//   }
+
+//   print() {
+//     let current = this.root;
+//     while (current) {
+//       console.log(current.value);
+//       current = current.next;
+//     }
+//   }
+// }
+
+// const numberList = new LinkedList<number>();
+
+// numberList.add(10);
+// numberList.add(5);
+// numberList.add(-3);
+
+// console.log(numberList.getNumberOfElements());
+// numberList.print();
+
+// const nameList = new LinkedList<string>();
