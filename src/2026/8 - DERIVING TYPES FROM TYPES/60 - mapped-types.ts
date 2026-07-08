b2dcdae8852bc02b58c349d86141e76d4870a03e
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-08 07:58:57 +0200

    review(2026): Deriving types from types - readonly types & optional mapping

--- src/2026/8 - DERIVING TYPES FROM TYPES/60 - mapped-types.ts
+++ src/2026/8 - DERIVING TYPES FROM TYPES/60 - mapped-types.ts
@@ -1,23 +1,55 @@
-type Operations = {
-  add: (a: number, b: number) => number;
-  substract: (a: number, b: number) => number;
-};
+// type Operations = {
+//   add: (a: number, b: number) => number;
+//   substract: (a: number, b: number) => number;
+// };
 
-// type Results = {
-//     add: number,
-//     substract:number
-// }
+// // type Results = {
+// //     add: number,
+// //     substract:number
+// // }
 
-type Results<T> = {
-  [Key in keyof T]: number;
-};
+// type Results<T> = {
+//   [Key in keyof T]: number;
+// };
 
-let mathOperations: Operations = {
-  add: (a: number, b: number) => a + b,
-  substract: (a: number, b: number) => a - b,
-};
+// let mathOperations: Operations = {
+//   add: (a: number, b: number) => a + b,
+//   substract: (a: number, b: number) => a - b,
+// };
 
-let mathResults: Results<Operations> = {
-  add: mathOperations.add(1, 2),
-  substract: mathOperations.substract(2, 1),
-};
+// let mathResults: Results<Operations> = {
+//   add: mathOperations.add(1, 2),
+//   substract: mathOperations.substract(2, 1), // if this will be removed the mathResults will yield typecheck error - what if we want more flexibility! thats what we will see in read only types & optional mapping down
+// };
+
+// Readonly Types & Optional Mapping
+// type Operations = {
+//   readonly add: (a: number, b: number) => number;
+//   readonly substract: (a: number, b: number) => number;
+//   //   add?: (a: number, b: number) => number;
+//   //   substract?: (a: number, b: number) => number;
+// };
+
+// // type Results = {
+// //     add: number,
+// //     substract:number
+// // }
+
+// type Results<T> = {
+//   //   [Key in keyof T]?: number;
+//   //   [Key in keyof T]-?: number;
+//   //   readonly [Key in keyof T]?: number;
+//   -readonly [Key in keyof T]?: number;
+// };
+
+// let mathOperations: Operations = {
+//   add: (a: number, b: number) => a + b,
+//   substract: (a: number, b: number) => a - b,
+// };
+
+// let mathResults: Results<Operations> = {
+//   add: mathOperations.add(1, 2),
+//   substract: mathOperations.substract(2, 1),
+// };
+
+// mathResults.add = 10;
