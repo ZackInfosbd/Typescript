a5c8ca6b1b49669873537827586622cc5a1c3043
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-09 08:40:46 +0200

    review(2026): EXPERIMENTAL TS Decorators - building more decorators

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,29 @@ function Logger(logString: string) {
   };
 }
 
-@Logger('LOGGING _ PERSON')
+function WithTemplate(template: string, hookId: string) {
+  return function (_: Function) {
+    const hookEl = document.getElementById(hookId);
+    if (hookEl) {
+      hookEl.innerHTML = template;
+    }
+  };
+}
+function WithTemplate2(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector('h1')!.textContent = p.name;
+    }
+  };
+}
+
+@Logger('LOGGING - PERSON')
+@WithTemplate2('<h1>My Person Object</h1>', 'app')
+@WithTemplate('<h1>My Person Object</h1>', 'app')
 class Person2 {
   name = 'zack';
 
