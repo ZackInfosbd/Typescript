34104974c8c69b371355abaec5f21dd1eb6ecf4e
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-09 08:02:59 +0200

    review(2026): EXPERIMENTAL TS Decorators - class and factory decorators

---
+++ src/2026/9 - DECORATORS/9-2 EXPERIMENTAL DECORATORS/70 - Class decorator.ts
@@ -0,0 +1,18 @@
+// function Logger(constructor: Function) {
+//   console.log('Logging...');
+
+//   console.log(constructor);
+// }
+
+// @Logger
+// class Person2 {
+//   name = 'zack';
+
+//   constructor() {
+//     console.log('creating a person object');
+//   }
+// }
+
+// const pers = new Person2();
+
+// console.log(pers);
---
+++ src/app.ts
@@ -0,0 +1,19 @@
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
+
+@Logger('LOGGING _ PERSON')
+class Person2 {
+  name = 'zack';
+
+  constructor() {
+    console.log('creating a person object');
+  }
+}
+
+const pers = new Person2();
+
+console.log(pers);
