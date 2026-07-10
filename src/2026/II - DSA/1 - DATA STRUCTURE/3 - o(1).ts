b22f5a9958f383bb3e0849a11ebab75bd878d9af
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-10 14:30:39 +0200

    review(2026): DSA - big O(n^2)

---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/3 - o(1).ts
@@ -0,0 +1,5 @@
+// // big 0(1)
+// const numbers: number[] = [1, 2, 3, 4, 5];
+// const getNumber = (arr: number[], index: number) => arr[index];
+
+// console.log(getNumber(numbers, 0));
---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/4 - o(n^2).ts
@@ -0,0 +1,37 @@
+// o(n^2)
+
+const numbers: number[] = [1, 2, 3, 4, 5];
+
+function findPairs(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      console.log(`Pair ${arr[i]},${arr[j]}`);
+    }
+  }
+}
+
+findPairs(numbers);
+
+// o(n^2) + n
+function findPairs2(arr: number[]) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j < arr.length; j++) {
+      console.log(`Pair ${arr[i]}, ${arr[j]}`);
+    }
+  }
+
+  for (let q = 0; q < arr.length; q++) {
+    console.log('-------------q', q);
+  }
+}
+
+findPairs2(numbers);
+
+/**
+ * If we combine all the "O" operations it becomes O(n^2 + 2).
+ * O(n^2) is a Dominat term.
+ * "n" is a non-Dominant term
+ * So we remove the "non-dominant" term and we're only left with O(n^2).
+ * this way, we simplify our bigO
+ 
+ */
