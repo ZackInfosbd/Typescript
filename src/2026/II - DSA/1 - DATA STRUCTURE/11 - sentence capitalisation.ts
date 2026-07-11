6832d46f98be1834b9ada1bfafbd87278f20da14
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-11 08:13:14 +0200

    review(2026): DSA - sentence capitalisation

---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/11 - sentence capitalisation.ts
@@ -0,0 +1,11 @@
+// // Capitalise word
+
+// const capitaliseWord = (text: string) => {
+//   return text
+//     .toLocaleLowerCase()
+//     .split(' ')
+//     .map((item: string) => item[0].toUpperCase() + item.slice(1))
+//     .join(' ');
+// };
+
+// console.log(capitaliseWord('hello amigo'));
