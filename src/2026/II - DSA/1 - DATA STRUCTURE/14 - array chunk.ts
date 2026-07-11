6e0b25ac4417dbd0e3bb2092afb967096320c820
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-11 09:06:14 +0200

    review(2026): DSA - array chunk & two sum

---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/14 - array chunk.ts
@@ -0,0 +1,21 @@
+// // Array Chunk
+
+// const arrOfNum: number[] = [];
+
+// const chunk = (arr: number[], size: number) => {
+//   const chunked: any[] = [];
+//   let index = 0;
+
+//   while (index < arr.length) {
+//     const chunk = arr.slice(index, index + size);
+
+//     console.log('---------', chunk);
+
+//     chunked.push(chunk);
+//     index += size;
+//   }
+
+//   return chunked;
+// };
+
+// console.log(chunk([1, 2, 3, 4, 5, 6, 7, 8], 3));
