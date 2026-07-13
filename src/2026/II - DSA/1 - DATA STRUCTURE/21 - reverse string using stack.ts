335e70ceebce404c1063a791e6575af110652066
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-13 10:01:13 +0200

    review(2026): DSA - reverse string using stack

---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/21 - reverse string using stack.ts
@@ -0,0 +1,19 @@
+// function reverseString(text: string) {
+//   const stack: string[] = [];
+
+//   for (let char of text) {
+//     stack.push(char);
+//   }
+
+//   let reversedStr = '';
+
+//   while (stack.length > 0) {
+//     reversedStr += stack.pop();
+//   }
+
+//   return reversedStr;
+// }
+
+// const reversedString = reverseString('hello world');
+
+// console.log(reversedString);
