ed55d64de4d0c4713551609ff4313b0208dbb9fb
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-13 11:31:32 +0200

    review(2026): DSA - word counter

---
+++ src/2026/II - DSA/1 - DATA STRUCTURE/23 - word counter.ts
@@ -0,0 +1,22 @@
+// function wordCounter(text: string) {
+//   const lowerText = text.toLocaleLowerCase();
+
+//   const wordMap: Record<string, number> = {};
+
+//   const words = lowerText.split(/\s+/);
+
+//   console.log(words);
+
+//   for (const word of words) {
+//     if (word in wordMap) {
+//       wordMap[word]++;
+//     } else {
+//       wordMap[word] = 1;
+//     }
+//   }
+//   return wordMap;
+// }
+
+// const phrase = 'Hello my name is is name zack';
+
+// console.log(wordCounter(phrase));
