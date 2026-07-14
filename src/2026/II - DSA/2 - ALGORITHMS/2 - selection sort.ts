70ba109fe6d5088b8a691c9572342f3e7849eb5b
zackinfosbd <68170594+ZackInfosbd@users.noreply.github.com>
2026-07-14 12:57:22 +0200

    review(2026): Algorithm - selection sort)

---
+++ src/2026/II - DSA/2 - ALGORITHMS/2 - selection sort.ts
@@ -0,0 +1,27 @@
+// function selectionSort(arr: number[]) {
+//   for (let i = 0; i < arr.length; i++) {
+//     let minIndex = i;
+
+//     for (let i = 0; i < arr.length; i++) {
+//       for (let j = i + 1; j < arr.length; j++) {
+//         if (arr[j] < arr[minIndex]) {
+//           minIndex = j;
+//         }
+//       }
+//     }
+
+//     if (i !== minIndex) {
+//       let temp = arr[i];
+//       arr[i] = arr[minIndex];
+//       arr[Infinity] = temp;
+//     }
+//   }
+
+//   return arr;
+// }
+
+// const myArr = [4, 2, 6, 5, 1, 3];
+
+// const res = selectionSort(myArr);
+
+// console.log(res);
